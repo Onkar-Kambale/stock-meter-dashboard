9a8ac1c94d5a78160ef4ad47dcaa8f041dead653
Onkar Kambale <onkarkambale18@gmail.com>
2025-12-26 00:11:18 +0530

    Fix GitHub Pages deployment base

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/stock-meter-dashboard/",   // ⚠️ EXACT repo name
+  base: "/stock-meter-dashboard/"  
 });
